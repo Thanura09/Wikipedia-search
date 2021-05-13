0ff35a038790212811baf82c1a3516d56a0be7e6
Thanura09 <thanura.marapana@gmail.com>
2021-05-13 10:25:52 +0530

    wiki app 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion from "./components/Accordion";
 
-function App() {
+const items = [
+  {
+    title: "What is react?",
+    content: "React is a front end JS library",
+  },
+  {
+    title: "Why use react?",
+    content: "Because it is easy to use",
+  },
+  {
+    title: "How to use react",
+    content: "Use react by creating components",
+  },
+];
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion items={items} />
     </div>
   );
-}
-
-export default App;
+};
